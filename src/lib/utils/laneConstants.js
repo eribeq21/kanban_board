4105978c0001f614624f94ae2a696ed033358150
Eriseldi <eribeq21@htl-shkoder.com>
2025-10-21 10:56:00 +0200

    Refactor parent page , create new utils file called LaneConstants

---
+++ src/lib/utils/laneConstants.js
@@ -0,0 +1,15 @@
+// Available lanes for the kanban board
+export const LANES = [
+    { name: 'Do', color: 'bg-blue-200/30' },
+    { name: 'Doing', color: 'bg-yellow-200/30' },
+    { name: 'Done', color: 'bg-green-200/30' },
+    { name: 'Archive', color: 'bg-gray-200/30' }
+];
+
+// Lane status names for easy reference
+export const LANE_STATUS = {
+    DO: 'Do',
+    DOING: 'Doing',
+    DONE: 'Done',
+    ARCHIVE: 'Archive'
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,14 +13,7 @@
 	import { getCountry } from '$lib/utils/geoUtils.js';
 	import { showDoneNotification } from '$lib/utils/notifications.js';
 	import { exportCSV } from '$lib/utils/exportUtils.js';
-
-	// Constants
-	const LANES = [
-		{ name: 'Do', color: 'bg-blue-200/30' },
-		{ name: 'Doing', color: 'bg-yellow-200/30' },
-		{ name: 'Done', color: 'bg-green-200/30' },
-		{ name: 'Archive', color: 'bg-gray-200/30' }
-	];
+	import { LANES } from '$lib/utils/laneConstants.js';
 
 	// State
 	let issues = $state([]);
